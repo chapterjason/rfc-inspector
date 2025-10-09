539dc59280941e5aac7613395e1deed23064a95f
chapterjason <jason.schilling@hotmail.de>
2025-10-09 20:59:40 +0200

    Add test TOC collection

--- packages/bin/src/index.ts
+++ packages/bin/src/index.ts
@@ -2,7 +2,7 @@ import * as fs from "node:fs/promises";
 import {mkdirSync, readFileSync} from "fs";
 import * as path from "node:path";
 import {mapWithConcurrency} from "./Utils/MapWithConcurrency.js";
-import {render, type Token, tokenize, TokenType,} from "@rfc-inspector/tokenizer";
+import {DataLineToken, render, type Token, tokenize, TokenType,} from "@rfc-inspector/tokenizer";
 import {ArrayCursor} from "@rfc-inspector/common";
 import {existsSync} from "node:fs";
 
@@ -17,6 +17,68 @@ const availableItems: number[] = JSON.parse(readFileSync(availableFile, 'utf-8')
 mkdirSync(rfcDirectory, {recursive: true});
 mkdirSync(outputDirectory, {recursive: true});
 
+/****/
+/****/
+/****/
+
+
+
+/****/
+/****/
+/****/
+
+function collectToc(text: string): Token[] {
+    const tokens = Array.from(tokenize(text));
+    const cursor = new ArrayCursor(tokens);
+    const tocTokens: Token[] = [];
+    let isInToc = false;
+
+    while (!cursor.isEOL()) {
+        const token = cursor.next();
+
+        if (token.type === TokenType.EOF) {
+            break;
+        }
+
+        if (token.type === TokenType.DATA_LINE && token.indent === 0) {
+            const previousToken = cursor.peek(-2);
+            const nextToken = cursor.peek(0);
+
+            if (previousToken?.type === TokenType.BLANK_LINE && nextToken?.type === TokenType.BLANK_LINE) {
+                if (isInToc) {
+                    isInToc = false;
+                    break;
+                }
+
+                if (token.data.toLowerCase().startsWith('table of contents')) {
+                    isInToc = true;
+                }
+            }
+        }
+
+        if (isInToc) {
+            if (token.type === TokenType.FORM_FEED_LINE) {
+                // remove previously added line as it is the page footer
+                tocTokens.pop();
+
+                // skip the next one
+                cursor.skip(1);
+
+                // skip this one
+                continue;
+            }
+
+            tocTokens.push(token);
+        }
+    }
+
+    return tocTokens;
+}
+
+/****/
+/****/
+/****/
+
 function ErrorHandler(error: Error) {
     console.error(error);
 }
@@ -26,37 +88,33 @@ async function main() {
         const filePath = path.join(rfcDirectory, `rfc${item}.txt`);
         const tocFilePath = path.join(outputDirectory, `rfc${item}_toc.txt`);
         const content = await fs.readFile(filePath, 'utf-8');
-        const tokens = Array.from(tokenize(content));
-        const cursor = new ArrayCursor(tokens);
 
-        // skip everything until line has thoses rules
-        // indent = 0 AND lastToken = BlankLine AND nextToken = BlankLine AND current line = DATA LINE
-        const tocTokens: Token[] = [];
-        let isInToc = false;
+        const items = new ArrayCursor(collectToc(content));
+        const tocTokens = [];
 
-        while (!cursor.isEOL()) {
-            const token = cursor.next();
+        while (!items.isEOL()) {
+            const item = items.next();
 
-            if (token.type === TokenType.DATA_LINE && token.indent === 0) {
-                const previousToken = cursor.peek(-2);
-                const nextToken = cursor.peek(0);
+            if (item.type === TokenType.EOF) {
+                break;
+            }
 
-                if (previousToken?.type === TokenType.BLANK_LINE && nextToken?.type === TokenType.BLANK_LINE) {
-                    if (isInToc) {
-                        isInToc = false;
-                        break;
-                    }
+            if (item.type === TokenType.BLANK_LINE) {
+                const peek = items.peekForwardUntil(next => next.type === TokenType.DATA_LINE);
 
-                    if (token.data.toLowerCase().startsWith('table of contents')) {
-                        isInToc = true;
-                    }
+                if (peek.length !== 0) {
+                    items.skip(peek.length);
+                    continue;
                 }
             }
 
-            if (isInToc) {
-                tocTokens.push(token);
-            }
+            tocTokens.push(item);
         }
+
+        // skip everything until line has thoses rules
+        // indent = 0 AND lastToken = BlankLine AND nextToken = BlankLine AND current line = DATA LINE
+
+        // replace 2+ consecutive blank lines
 
         const text = render(tocTokens).trim();
 
